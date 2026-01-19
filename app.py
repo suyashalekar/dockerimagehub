7d23453d35f249b8bb538a39d17301f1a3821583
suyashalekar <suyashalekar519@gmail.com>
2026-01-19 20:27:46 +1100

    change the name in app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return "Suyash Alekar Hello!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port = 5000)
-    
+
